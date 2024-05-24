0ea35a233179b7bfdef65488f28876e9ea606b78
chineducode <chinedunnaemeka2@gmail.com>
2024-05-24 16:02:02 +0100

    port number added

--- backend/server.js
+++ backend/server.js
@@ -155,5 +155,5 @@ io.on('connection', async (socket)=> {
     }
 })
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, ()=> console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
